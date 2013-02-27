0e868079655c6693037f9a4d122fd7b86ccb9b75
Karthik Ram <karthik.ram@gmail.com>
2013-02-26 16:57:04 -0800

    updated namespace, renamed observation
    To avoid generic names for functions.

--- R/observations.R
+++ R/observations.R
@@ -9,7 +9,7 @@
 #' @examples \dontrun{
 #' observation()
 #'}
-observations <- function(type = NULL) {
+bee_observations <- function(type = NULL) {
  base_url <- "http://ecoengine.berkeley.edu/api/"
  url <- paste0(base_url, "?format=json")
  # Will flesh this out next when I get a bigger chunk of time
