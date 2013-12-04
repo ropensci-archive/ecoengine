2cfb4a8333202aecf243dab3e8f96bbe4689a9a2
Karthik Ram <karthik.ram@gmail.com>
2013-12-04 14:19:31 -0800

    Photos is complete. Fixes #10

--- R/photos.R
+++ R/photos.R
@@ -15,6 +15,7 @@
 #' @param  related_type Need to describe these parameters
 #' @param  related  Need to describe these parameters
 #' @param  other_catalog_numbers Need to describe these parameters
+#' @param  verbose Default is \code{TRUE}. Set to \code{FALSE} to suppress output.
 #' @param  foptions = list() Other options to pass to curl
 #' @export
 #' @importFrom httr stop_for_status content GET
@@ -34,10 +35,10 @@ holos_photos <- function(page = NULL,
 						 min_date = NULL, 
 						 max_date = NULL, 
 						 related_type = NULL, 
-						 related  = NULL, 
+						 related  = NULL,
+						 succinct = FALSE,
 						 other_catalog_numbers = NULL, 
 						 foptions = list()) {
-						 # function isn't ready.	
 
 	photos_url <- "http://ecoengine.berkeley.edu/api/photos/?format=json"
 	# Some thoughts. I can check if page is integer
@@ -62,10 +63,13 @@ holos_photos <- function(page = NULL,
     stop_for_status(data_sources)
     photos <- content(data_sources)
     page_num <- ifelse(is.null(page), 1, page)
-    message(sprintf("Search returned %s photos \n page %s of %s", photos[[1]], page_num, ceiling(photos[[1]]/10)))
+    if(!succinct) {
+    message(sprintf("Search returned %s photos (page %s of %s)", photos[[1]], page_num, ceiling(photos[[1]]/10)))
+	}
     photos_data <- do.call(rbind, photos[[4]])
-    photos_data
-
+    photos_results <- list(results = photos[[1]], call = photos[[2]], type = "photos", data = photos_data)
+    class(photos_results) <- "holos"
+    return(photos_results)
 	# possible outputs:
 	# 	author
 	# 	date_range
@@ -75,12 +79,61 @@ holos_photos <- function(page = NULL,
 	# 	associated_observations
 }
 
+
+
+#'holos_photos_get
+#'
+#'This wrapper around holos photos. Allows a user to retrive all data at once for a query rather than request a page at a time.
+#' @param page Use \code{all} to request all pages for a particular query.
+#' @param ... All the arguments that go into \code{holos_photos}
+#' @export
+#' @examples \dontrun{
+#' all_cdfa <- holos_photos_get(collection_code = "CDFA", page = "all")
+#'}
+holos_photos_get <- function(..., page = NULL) {
+	if(page == "all") {
+		x <- holos_photos(..., succinct = TRUE)
+		total_results <- x$results
+		# Then ldply through x
+		result_list <- list()
+		all_pages <- ceiling(total_results/10)
+		if(total_results > 1000) {
+		message(sprintf("Retrieving %s (%s requests) results. This may take a while", total_results, ceiling(total_results/10)))
+		}
+		for(i in 1:all_pages) {
+		result_list[[i]] <- holos_photos(..., page = i)$data
+		if(i %% 10 == 0) Sys.sleep(2)
+		}
+		result_data <- as.data.frame(do.call(rbind, result_list))
+		all_photo_results <- list(results = x[[1]], call = x[[2]], type = "photos", data = result_data)
+		class(all_photo_results) <- "holos"
+
+	} else { 
+		all_photo_results <- holos_photos(...)
+	}
+
+# Return all results
+	all_photo_results
+}
+
+# -------------------------
 #  A few local tests
 holos_photos()
 holos_photos(collection_code = "CalAcademy")
 holos_photos(county = "Santa Clara County")
 holos_photos(county = "Merced County")
+holos_photos(author = "Charles Webber")
+# Don't have an example for remote_id
+holos_photos(collection_code = "CalAcademy")
+holos_photos(collection_code = "VTM")
+holos_photos(collection_code = "CalFlora")
+holos_photos(collection_code = "CDFA")
+all_cdfa <- holos_photos_get(collection_code = "CDFA", page = "all")
 
+# have no idea what sources are
+
+
+# -------------------
 # To do:
 # Implement search
 # Return nicely formatted tables of data
