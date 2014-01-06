4ce6a9200b0318c8432aae213ac56d9688db33ce
Karthik Ram <karthik.ram@gmail.com>
2014-01-05 21:38:51 -0800

    Fixed bad no documentation tag.

--- R/checklists.R
+++ R/checklists.R
@@ -57,7 +57,7 @@ details_data <- content(details)
 return(ldply(details_data$observations))
 }
 
- #' @nord
+#' @noRd
 eco_capwords <- function(s, strict = FALSE, onlyfirst = FALSE) {
         cap <- function(s) paste(toupper(substring(s,1,1)),
                 {s <- substring(s,2); if(strict) tolower(s) else s}, sep="", collapse=" " )
