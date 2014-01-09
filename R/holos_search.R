3f035d46937b4a3fd55c9573310c891064adaa24
Karthik Ram <karthik.ram@gmail.com>
2014-01-09 15:09:29 -0800

    Adding solr search.

--- R/holos_search.R
+++ R/holos_search.R
@@ -1,6 +1,23 @@
 
 
-holos_search <- function() {
-	# Holos search place holder
-	# Still to be coded
+#' Holos search
+#'
+#' Search the ecoengine
+#' @param query search term
+#' @param facet Field to facet on. Possibilities include: kingdom, genus, family, clss, phylum
+#' @param  foptions = list() Additional (optional) arguments to httr
+#' @export
+#' @importFrom solr solr_facet
+#' @examples \dontrun{
+#' holos_search(query = "Pinus", facet = "Genus")
+#'}
+holos_search <- function(query = NULL, facet = NULL, foptions = list()) {
+search_url <- "http://ecoengine.berkeley.edu/api/search/"
+browser()
+# this fails
+result <- solr_facet(q = query, facet.field = facet, url = search_url)
 }
+
+# Notes
+# search_url <- "http://ecoengine.berkeley.edu/api/search/?q=genus:Lynx"
+# This API call (just the URL) wil work in browser
