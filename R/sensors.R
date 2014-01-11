e77e06a1d66a0411d804d0126adadaa556b79781
Karthik Ram <karthik.ram@gmail.com>
2014-01-11 12:58:36 -0800

    Updated sensors.

--- R/sensors.R
+++ R/sensors.R
@@ -4,6 +4,7 @@
 #'
 #' Returns UC reserve system sensor data
 #' @param page page number
+#' @param page_size Number of observations per page
 #' @param  remote_id Need to describe these parameters
 #' @param  collection_code Type of collection. Can be \code{CalAcademy}, \code{Private}, \code{VTM}, \code{CDFA}. \code{CalFlora} Others TBA 
 #' @param  source  Need to describe these parameters
@@ -16,6 +17,7 @@
 #' ee_sensors_get(page = 40)
 #'}
 ee_sensors_get <- function(page = NULL, 
+                        page_size = 25,
 						remote_id = NULL, 
 						collection_code = NULL, 
 						source = NULL, 
@@ -24,7 +26,8 @@ ee_sensors_get <- function(page = NULL,
 						foptions = list()) {
 
 sensor_url <- "http://ecoengine.berkeley.edu/api/sensors/?format=json"
-    args <- as.list(compact(c(page = page,                       
+    args <- as.list(compact(c(page = page,
+                              page_size = page_size,                       
                             remote_id = remote_id, 
                             collection_code = collection_code, 
                             source  = source , 
@@ -43,18 +46,20 @@ sensor_url <- "http://ecoengine.berkeley.edu/api/sensors/?format=json"
 #'
 #'Returns a full list of sensors with available data
 #' @param ... same arguments as \code{\link{ee_sensors}}
+#' @param page_size Number of observations per page
 #' @export
 #' @return data.frame
 #' @examples \dontrun{
+#' # This call will return a full set of available sensors.
 #' full_sensor_list <- ee_sensors()
 #'}
-ee_sensors <- function(...) {
+ee_sensors <- function(..., page_size = 25) {
     x <- content(GET("http://ecoengine.berkeley.edu/api/sensors/?format=json"))
     total_results <- x$count
-    all_available_pages <- ceiling(total_results/10) 
+    all_available_pages <- ceiling(total_results/page_size) 
     all_results <- list()       
     for(i in seq(all_available_pages)) {
-        all_results[[i]] <- ee_sensors_get(page = i)
+        all_results[[i]] <- ee_sensors_get(page = i, page_size = page_size)
     }
     return(ldply(all_results))
 }
