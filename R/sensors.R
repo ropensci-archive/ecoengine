89717f71d08c1f6b1731ad29bff689c8dfdc0edf
Karthik Ram <karthik.ram@gmail.com>
2014-01-13 12:09:40 -0800

    Sensor data can now be correctly retrieved. fixes #7

--- R/sensors.R
+++ R/sensors.R
@@ -63,3 +63,82 @@ ee_sensors <- function(..., page_size = 25) {
     }
     return(ldply(all_results))
 }
+
+# ----------------------------------------
+# x <- full_sensor_list[1, ]$data_url
+
+#' sensor data
+#'
+#' Request data from any of the sensors binned at various intervals
+#' @param data_url The URL returned by \code{\link{ee_sensors}}
+#' @param  page  Requested page. Can specify a single page (e.g. \code{1}), a range (\code{1:2}), or "all"
+#' @param  page_size Default number of observations per page is 25.
+#' @param  foptions Additional arugments to httr
+#' @export
+#' @seealso \code{\link{ee_sensors})}
+#' @examples \dontrun{
+#' full_sensor_list <- ee_sensors()
+#' x <- full_sensor_list[1, ]$data_url
+#' sensor_data <- ee_sensor_data(data_url = x, page = 1:2)
+#'}
+ee_sensor_data <- function(data_url = NULL, page = NULL, page_size = 25, foptions = list()) {
+if(is.null(page)) {
+    stop("Page has to be a range or all")
+}
+
+data_url <- paste0(data_url, "?format=json")
+data <- GET(data_url)
+stop_for_status(data)
+data_list <- content(data)
+
+total_obs <- data_list$count
+page_size <- page_size
+total_pages <- ceiling(total_obs/page_size)
+req_pages <- seq_along(1:total_pages)
+
+
+if(identical(class(page), "numeric") | identical(class(page), "integer")) {
+    largest_page <- max(page)
+    if(largest_page > total_pages) stop("One or more of the requested pages is out of range")
+    total_pages <- length(page)
+    req_pages <- seq_along(page)
+ }
+
+if(total_pages > 100) {
+    message(sprintf("You are requesting a large number of pages (%s total). This might cause the server to stop responding. It would be wise to break up the request, either through multiple requests or via parallel execution. See help file for examples", total_pages))
+}
+    results <- list()
+    for(i in seq_along(req_pages)) {
+        args <- compact(list(page = i, page_size = page_size))
+        temp_data <- GET(data_url, query = args)
+        stop_for_status(temp_data)
+        sensor_raw <- content(temp_data)$results
+        results[[i]] <- ldply(sensor_raw, function(x) {
+            cbind(data.frame(t(unlist(x[1:2]))), data.frame(t(unlist(x[3]))))
+        })
+    }
+    final_results <- do.call(rbind, results)
+    sensor_data <- list(results = total_obs, call = data_list[[2]], type = "sensor", data = final_results)
+    class(sensor_data) <- "ecoengine"
+    sensor_data
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
