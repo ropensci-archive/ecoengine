bea64f9884d6a984e975a32c2ab56706e4995b87
Karthik Ram <karthik.ram@gmail.com>
2014-01-14 14:41:51 -0800

    added date support #30

--- R/sources.R
+++ R/sources.R
@@ -4,6 +4,7 @@
 #' @template foptions
 #' @export
 #' @importFrom httr GET content stop_for_status
+#' @importFrom lubridate ymd_hms
 #' @return \code{data.frame}
 #' @examples \dontrun{
 #' source_list <- ee_sources()
@@ -13,7 +14,9 @@ ee_sources <- function(foptions = list()) {
     data_sources <- GET(base_url, foptions)
     stop_for_status(data_sources)
     ds <- content(data_sources)
-    as.data.frame(do.call(rbind.data.frame, ds$results))
+    sources <- as.data.frame(do.call(rbind.data.frame, ds$results))
+    sources$retrieved <- ymd_hms(sources$retrieved)
+    sources
 }
 
 
