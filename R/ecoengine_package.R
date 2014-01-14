fda73a9a40b8f6c64d583f479d9ddd32743c10ed
Karthik Ram <karthik.ram@gmail.com>
2014-01-14 14:44:39 -0800

    Added documentation for the sensor list dataset. This allows for querying sensor data based on location name.

--- R/ecoengine_package.R
+++ R/ecoengine_package.R
@@ -16,3 +16,14 @@ NULL
 #' @usage data(california_counties)
 #' @format A data table with 58 rows and 3 variables. Contains county name, FIPS state code (06 in this case) and FIPS county code. See \href{http://en.wikipedia.org/wiki/FIPS_county_code}{Wikipedia} for more information.
 NULL
+
+#' full_sensor_list
+#'
+#' A data.frame containing all the sensors currently available through the engine. This list can be updated anytime by calling the ee_sensors() function. 
+#' @docType data
+#' @keywords datasets
+#' @name full_sensor_list
+#' @usage data(full_sensor_list)
+#' @format A data table with 40 rows and 13 variables. Contains units, variable, data_url, source, record, site_code,   method_name, begin_date, end_date, station_name, geojson.type, geojson.coordinates1, geojson.coordinates2
+NULL
+
