24bd14aa3af7450871cdcc12c438077f30c7661c
Karthik Ram <karthik.ram@gmail.com>
2014-01-15 12:54:40 -0800

    Completed sensor aggregations. fixes #28

--- R/sensors_aggregation.R
+++ R/sensors_aggregation.R
@@ -1,21 +1,76 @@
 
 #'sensor aggregation
 #'
-#' Aggregated sensor data
+#' Aggregated sensor_id ID of desired sensor. See \code{\link{ee_list_sensors}} for a list.
 #' @template pages
 #' @template dates
-#' @param interval .......
+#' @param sensor_id The id of the sensor. 
+#' @param hours Time interval in hours
+#' @param minutes Time interval in minutes
+#' @param seconds Time interval in seconds
+#' @param days Time interval in days
+#' @param weeks Time interval in weeks
+#' @param month Time interval in months
+#' @param years Time interval in years
+#' @param quiet Default is \code{FALSE}. Set to \code{TRUE} to suppress messages.
 #' @template foptions
 #' @export
 #' @examples \dontrun{
-#' ee_sensor_agg(min = "1994-04-04", max="2001-01-01")
+#' aggregated_data <-  ee_sensor_agg_get(sensor_id = 1625, weeks = 2)
 #'}
-ee_sensor_agg <- function(page = NULL, page_size = 25, interval = NULL, min_date = NULL, max_date = NULL, foptions = list()) {
-	# to be coded.
+ee_sensor_agg_get <- function(sensor_id = NULL, page = NULL, page_size = 25, hours = NULL, minutes = NULL, seconds = NULL, days = NULL, weeks = NULL, month = NULL, years = NULL, min_date = NULL, max_date = NULL, quiet = FALSE, foptions = list()) {
+
+if(is.null(sensor_id)) {
+	stop("Sensor ID required. use ee_list_sensors() to obtain a full list of sensors")
 }
 
-# you can use "interval=x" where x can be years, months, weeks, days, or hours. There is also a way to define a custom range but I'm not sure how that works.
+page <- ifelse(is.null(page), 1, page)
+sensor_agg_url <- paste0("http://ecoengine.berkeley.edu/api/sensors/", sensor_id, "/aggregate/?format=json")
+interval <- as.list(compact(c(H = hours,  T = minutes, S = seconds, D = days, W = weeks, M = month, Y = years)))
+paste_names <- function(interval_name, value) { paste(interval_name, value, collapse = "", sep = "") }
+interval <- llply(interval, paste_names, names(interval))
+args <- as.list(compact(c(page = page, page_size = 25, min_date = min_date,  interval = interval[[1]], max_date = max_date)))
+sensor_call <- GET(sensor_agg_url, query = args, foptions)
+stop_for_status(sensor_call)
+sensor_res <- content(sensor_call)
+if(!quiet) {
+	message(sprintf("Retrieving page %s (%s observations total)\n", page, sensor_res$count))
+}
+if(sensor_res$count == 0)
+	return(NULL) else {
+		sensor_res_list <- llply(sensor_res$results, function(x) {
+			 lapply(x, function(z) { ifelse(is.null(z),"NA", z) })
+		})
+		sensor_data_agg <- do.call(rbind.data.frame, (lapply(sensor_res_list, LinearizeNestedList)))
+		sensor_results <- list(results = sensor_res$count, call = sensor_res[[2]], type = "sensor", data = sensor_data_agg)
+    	class(sensor_results) <- "ecoengine"
+		sensor_results
+		}
+}
+# [TODO] Now to build a function to paginate aggregate calls
 
-# For example, here is the URL for "Angelo Meadow WS - Solar Radiation Total MJ/m^2" by week:
+#' Ecoengine sensor aggregations
+#'
+#'<full description>
+#' @param ... all the arguments that get passed to \code{\link{ee_sensor_agg_get}}
+#' @export
+#' @examples \dontrun{
+#' sensor_df <- ee_sensor_agg(sensor_id = 1625, weeks = 2)
+#' sensor_df <- ee_sensor_agg(page = "all", sensor_id = 1625, weeks = 2)
+#'}
+ee_sensor_agg <- function(...) {
+	ee_get(..., input_fn = ee_sensor_agg_get, dtype = "sensor")
+}
 
-# http://ecoengine.berkeley.edu/api/sensors/1629/aggregate/?interval=weeks
+#' Lists subset of the full sensor list
+#'
+#' @examples \dontrun{
+#' ee_list_sensors()
+#'}
+ee_list_sensors <- function() {
+data(full_sensor_list)
+full_sensor_list[, c("station_name", "units", "variable", "method_name", "record")]	
+}
+
+
+
