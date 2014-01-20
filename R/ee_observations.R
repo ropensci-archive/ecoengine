d0a566d935ee2c26822184037f28268bee5fe929
Karthik Ram <karthik.ram@gmail.com>
2014-01-20 13:38:50 -0800

    lat/long are correctly labeled in the results.

--- R/ee_observations.R
+++ R/ee_observations.R
@@ -96,7 +96,8 @@ if(progress) pb <- txtProgressBar(min = 0, max = length(required_pages), style =
     }
     
 	obs_data_all <- do.call(rbind, results)
-
+    names(obs_data_all)[which(names(obs_data_all)=="geojson.coordinates1")] <- "latitude"
+    names(obs_data_all)[which(names(obs_data_all)=="geojson.coordinates2")] <- "longitude"
 observation_results <- list(results = obs_data$count, call = main_args, type = "observations", data = obs_data_all)
 
 class(observation_results) <- "ecoengine"
