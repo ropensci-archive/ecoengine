a0dfb7a39653b4cf765593bc3313106076b34c80
Karthik Ram <karthik.ram@gmail.com>
2014-08-12 15:47:31 -0700

    Hid slower examples

--- R/ee_browse_photos.R
+++ R/ee_browse_photos.R
@@ -10,7 +10,7 @@
 #' @examples 
 #' # view_photos(ee_photos())
 #' # Pictures of racoons
-#' view_photos(ee_photos(scientific_name = "Procyon lotor", quiet = TRUE))
+#' # view_photos(ee_photos(scientific_name = "Procyon lotor", quiet = TRUE))
 #' # or the California Condor
 #' # view_photos(ee_photos(scientific_name = "Gymnogyps californianus", quiet = TRUE))
 
